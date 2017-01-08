6464b35c1f0a17fd97f165be94c6e69d4b219a85
Michal Migurski <mike@teczno.com>
2017-01-08 11:45:38 -0800

    Added skeletal chef-solo setup so this is easier in the future

---
+++ chef/dotmaps/recipes/default.rb
@@ -0,0 +1,3 @@
+package 'unzip'
+package 'gdal-bin'
+package 'postgresql-9.3-postgis-2.1'
---
+++ chef/solo.rb
@@ -0,0 +1,10 @@
+#
+# http://docs.opscode.com/config_rb_solo.html
+#
+file_cache_path  "/tmp/chef-solo"
+cookbook_path    File.dirname(__FILE__)
+data_bag_path    File.dirname(__FILE__)
+log_level        :info
+log_location     "/var/log/cheflog"
+ssl_verify_mode  :verify_none
+
