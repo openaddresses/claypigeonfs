139f8f39f450801bcd54e1bdb4b6606433b0e968
Michal Migurski <mike@teczno.com>
2017-01-09 20:46:32 -0800

    Added basic Mapzen map showing blue dots
    Dot design copied from http://dotspotting.org

--- Tiler/__init__.py
+++ Tiler/__init__.py
@@ -1,9 +1,18 @@
 import os, TileStache, ModestMaps
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 from .tippecanoe import Config
 
 app = Flask(__name__)
 config = Config('.')
+
+@app.route('/')
+def get_index():
+    return render_template('index.html')
+
+@app.route('/scene.yaml')
+def get_scene():
+    return Response(render_template('scene.yaml'),
+        headers={'Content-Type': 'application/x-yaml'})
 
 @app.route('/<int:zoom>/<int:col>/<int:row>.<ext>')
 @app.route('/<int:zoom>/<int:col>/<int:row>')
