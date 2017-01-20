d421810ca94b99c8ce4277240fa942eae4f285a1
Michal Migurski <mike@teczno.com>
2017-01-19 20:50:52 -0800

    Added Claypigeon Python package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from os.path import join, dirname
+import sys
+
+with open(join(dirname(__file__), 'Claypigeon', 'VERSION')) as file:
+    version = file.read().strip()
+
+setup(
+    name = 'Claypigeon',
+    version = version,
+    url = 'https://github.com/openaddresses/claypigeonfs',
+    author = 'Michal Migurski',
+    author_email = 'mike-pypi@teczno.com',
+    description = 'FUSE filesystem for remote ranged HTTP files.',
+    packages = ['Claypigeon'],
+    entry_points = dict(
+        console_scripts = [
+            'claypigeon-mount = Claypigeon.filesystem:main',
+        ]
+    ),
+    package_data = {},
+    test_suite = 'Claypigeon.tests',
+    install_requires = [
+        'llfuse == 1.1.1',
+        'requests == 2.12.4',
+        ]
+)
