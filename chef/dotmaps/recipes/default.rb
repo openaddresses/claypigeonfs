085757285040b6db4ef894df380f923f9d6f5f52
Michal Migurski <mike@teczno.com>
2017-01-19 20:52:55 -0800

    Removed non-Claypigeon files

--- chef/dotmaps/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-package 'unzip'
-package 'gdal-bin'
-package 'postgresql-9.3-postgis-2.1'
-package 'python-virtualenv'
-
-package 'pkg-config'
-package 'libfuse-dev'
-package 'python3-dev'
-package 'libattr1-dev'
--- serve-tiles.py
+++
@@ -1,4 +0,0 @@
-from Tiler import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
